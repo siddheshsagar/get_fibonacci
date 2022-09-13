20a3d024baace2afd77a6f0d46eb821f69c388af
Siddhesh Sagar <77565933+siddheshsagar@users.noreply.github.com>
2022-09-13 14:25:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
 from setuptools import setup, find_packages
  
-classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
- 
 setup(
   name='cal_fibonacci',
   version='0.0.1',
@@ -17,7 +9,6 @@ setup(
   author='Siddhesh Sagar',
   author_email='siddheshsagar3182001@gmail.com',
   license='MIT', 
-  classifiers=classifiers,
   keywords='fibonacci', 
   packages=find_packages(),
   install_requires=[''] 
