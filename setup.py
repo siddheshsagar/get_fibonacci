8a8cc4888272b2568175dd77dd06a7f1e3776951
Siddhesh Sagar <77565933+siddheshsagar@users.noreply.github.com>
2022-09-13 15:02:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
   name='cal_fibonacci',
   version='0.0.1',
   description='A very basic fibonacci calculator',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='https://github.com/siddheshsagar/get_fibonacci.git',
   author='Siddhesh Sagar',
   author_email='siddheshsagar3182001@gmail.com',
