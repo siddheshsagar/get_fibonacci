899ee2b8fd864323a40d17034f5c865f53d91d5d
siddhesh <siddheshsagar3182001@gmail.com>
2022-09-13 13:34:55 +0530

    added all the files to repo

---
+++ __init__.py
@@ -0,0 +1,14 @@
+def calculate_fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+    a,b = 1,1
+    for i in range(2,n):
+        temp = a+b 
+        a = b
+        b = temp
+    return temp
+
+
+print(calculate_fibonacci(6))
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='cal_fibonacci',
+  version='0.0.1',
+  description='A very basic fibonacci calculator',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Siddhesh Sagar',
+  author_email='siddheshsagar3182001@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='fibonacci', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
